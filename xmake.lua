4296b15b11eb9e79e4b75eba615247a2e70cd356
Kong <xykong421@outlook.com>
2024-02-12 17:15:26 +0800

    Update xmake.lua

--- xmake.lua
+++ xmake.lua
@@ -5,6 +5,6 @@ target("test_xmake_add_defines_windows_path")
     set_kind("binary")
     add_files("src/*.cpp")
     add_defines("TEST_PATH_1=\"$(projectdir)\"")
-    
-    local model_path="$(projectdir)"
-    add_defines("TEST_PATH_2=\"" .. path.cygwin_path(model_path) .. "\"")
+
+    local model_path= "$(projectdir)"
+    add_defines("TEST_PATH_2=\"" .. model_path:gsub("\\", "\\\\") .. "\"")
