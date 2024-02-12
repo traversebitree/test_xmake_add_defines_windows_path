02adbdeb604670cfbb80aa225ade48b7c415a5e2
Kong <xykong421@outlook.com>
2024-02-12 16:40:35 +0800

    Update xmake.lua

--- src/main.cpp
+++ src/main.cpp
@@ -4,6 +4,7 @@ using namespace std;
 
 int main(int argc, char **argv) {
   cout << "hello world!" << endl;
-  cout << TEST_PATH << endl;
+  cout << TEST_PATH_1 << endl;
+  cout << TEST_PATH_2 << endl;
   return 0;
 }
--- xmake.lua
+++ xmake.lua
@@ -4,4 +4,7 @@ add_rules("plugin.compile_commands.autoupdate", {outputdir = "."})
 target("test_xmake_add_defines_windows_path")
     set_kind("binary")
     add_files("src/*.cpp")
-    add_defines("TEST_PATH=\"$(projectdir)\"")
+    add_defines("TEST_PATH_1=\"$(projectdir)\"")
+    
+    local model_path="$(projectdir)"
+    add_defines("TEST_PATH_2=\"" .. path.cygwin_path(model_path) .. "\"")
